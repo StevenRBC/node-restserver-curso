5acd70fe9e0c35ad58be740a6975c16aa417be83
Steven <warriorsteb@gmail.com>
2021-01-21 19:40:28 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -7,7 +7,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://Stevenrbc:ctGiYhyMPOcmFYsU@cluster0.gg4rg.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
